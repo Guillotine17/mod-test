c829957b9a5706031106204bcc458ac74318b498
Sean Bryan <bryan.sean@gmail.com>
2023-09-28 14:50:31 -0400

    deece

---
+++ src/components/postDetail.tsx
@@ -0,0 +1,13 @@
+import { Post } from "@/types/posts";
+
+export default function PostDetail({ post }: { post: Post }) {
+    return (
+
+        <div className='container py-2 px-2 flex flex-col'>
+            <h1 className='underline text-2xl font-bold decoration-sky-500'>{post.title}</h1>
+            <p style={{
+                "whiteSpace": "pre-wrap"
+            }}>{post.body}</p>
+        </div>
+    )
+}
